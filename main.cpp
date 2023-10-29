2f574fe995228ff08b52030edc5358953f4c6752
Mahmoud Elsherif <moelsherif@google.com>
2023-10-29 04:20:36 +0000

    make project dynamic for multiple html files

--- main.cpp
+++ main.cpp
@@ -4,17 +4,43 @@
 #include <netinet/in.h>
 #include <unistd.h>
 #include <fstream>
+#include <sstream>
 
 std::string get_file_content(const std::string& file_path) {
     std::ifstream file(file_path);
     if (!file.is_open()) {
-        std::cerr << "Could not open the file!" << std::endl;
+        std::cerr << "Could not open the file: " << file_path << std::endl;
         return "";
     }
 
     std::string content((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
     file.close();
     return content;
+}
+
+std::string get_requested_file_path(const std::string& request) {
+    std::istringstream request_stream(request);
+    std::string method, path, protocol;
+    request_stream >> method >> path >> protocol;
+    std::cout << "Method: " << method << std::endl;
+    std::cout << "Path: " << path << std::endl;
+    std::cout << "Protocol: " << protocol << std::endl;
+    
+    if (method != "GET") {
+        return "";
+    }
+    
+    if (path == "/") {
+        path = "/index.html";
+    }
+
+    // Preventing directory traversal attacks by removing ".." from path
+    size_t pos;
+    while ((pos = path.find("..")) != std::string::npos) {
+        path.erase(pos, 2);
+    }
+
+    return "." + path;  // Prepending a dot to construct the relative file path
 }
 
 int main() {
@@ -64,13 +90,19 @@ int main() {
         }
 
         std::cout << "Request:\n" << buffer << std::endl;
+        
+        std::string file_path = get_requested_file_path(buffer);
+        std::string file_content = get_file_content(file_path);
+        
+        if (file_content.empty()) {
+            std::string not_found_response = "HTTP/1.1 404 Not Found\nContent-Type: text/plain\nContent-Length: 9\n\nNot Found";
+            send(new_socket, not_found_response.c_str(), not_found_response.length(), 0);
+        } else {
+            std::string response = "HTTP/1.1 200 OK\nContent-Type: text/html\nContent-Length: " + std::to_string(file_content.length()) + "\n\n" + file_content;
+            send(new_socket, response.c_str(), response.length(), 0);
+        }
 
-        // Assuming the HTML file is named "index.html" and is located in the same directory as the executable
-        std::string html_content = get_file_content("index.html");
-        std::string response = "HTTP/1.1 200 OK\nContent-Type: text/html\nContent-Length: " + std::to_string(html_content.length()) + "\n\n" + html_content;
-
-        send(new_socket, response.c_str(), response.length(), 0);
-        std::cout << "HTML content sent\n" << std::endl;
+        std::cout << "Response sent\n" << std::endl;
 
         close(new_socket);
     }
